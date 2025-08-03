2c4978caee40c1f94cba8192b4ebf051c9e2070b
Andreina Di Brigida <Akiarakirax3@gmail.com>
2025-08-02 22:18:20 -0400

    Add proyect

---
+++ js/index.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.querySelector(".mobile-menu-button");
+  const menu = document.querySelector(".mobile-menu");
+
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("visible");
+  });
+
+  const headerMenu = document.querySelector(".header-menu");
+  const imageHero = document.querySelector(".image-hero");
+
+  function toggleHeaderMenu() {
+    if (window.innerWidth >= 1024) {
+      headerMenu.classList.remove("hidden");
+      imageHero.classList.remove("hidden");
+    } else {
+      headerMenu.classList.add("hidden");
+      imageHero.classList.add("hidden");
+    }
+  }
+
+  window.addEventListener("resize", toggleHeaderMenu);
+  toggleHeaderMenu(); // Ejecuta al cargar
+
+  new Splide(".splideTestimonials", {
+    type: "loop",
+    perPage: 1,
+    drag: "free",
+    snap: true,
+    focus: "center",
+    isLoop: true,
+    gap: "20px",
+    padding: {
+      left: "20%",
+      right: "35%",
+    },
+    pagination: true,
+    breakpoints: {
+      1024: {
+        perPage: 1,
+        arrows: true,
+      },
+      768: {
+        perPage: 1,
+        arrows: true,
+      },
+      640: {
+        perPage: 1,
+        padding: {
+          left: "0%",
+          right: "0%",
+        },
+        arrows: false,
+      },
+    },
+    updateOnMove: true,
+    autoplay: true,
+    interval: 3000,
+  }).mount();
+});
